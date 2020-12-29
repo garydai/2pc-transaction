dafbd2b448a218333a9ccb32cad2b9205aad85fe
daitechang <techang.dai@ecreditpal.com>
2020-12-29 11:16:24 +0800

    init

---
+++ server2/src/main/java/com/luban/DemoService.java
@@ -0,0 +1,22 @@
+package com.luban;
+
+import com.luban.annotation.LbTransactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DemoService {
+
+    @Autowired
+    private DemoDao demoDao;
+
+
+    @LbTransactional(isEnd = true)
+    @Transactional
+    public void test() {
+        System.out.println("调用了");
+        demoDao.insert("server2");
+//        int i = 1/0;
+    }
+}
